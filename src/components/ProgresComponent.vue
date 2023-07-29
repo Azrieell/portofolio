4b300d03bbba75c45072b578a8c504d488ec3d05
Azrieell <21221753106@smkassalaambandung.sch.id>
2023-07-29 11:26:13 +0700

    Web saya

--- src/components/ProgresComponent.vue
+++ src/components/ProgresComponent.vue
@@ -1,3 +1,25 @@
 <template>
+  <center>
+      <h1
+        style="font-size: 20px; margin-bottom: 15px; margin-top: 0px; margin-left: 40px; font-family: 'Courier New', Courier, monospace; back bac">
+        SKILLS</h1>
+    </center>
+    <center>
+      <div style="width: 10%; background: grey; height: 4px; margin-left: 40px;"></div>
+    </center>
+    <br>
+    <br>
+    <br>
+  <div style="padding-left: 50px; margin-left: 190px;" class="font-medium">
+    <div class="grid grid-cols-2 md:grid-cols-3 gap-4">
+    <span class="flex items-center text-sm font-medium text-gray-900 dark:text-white"><span class="flex w-2.5 h-2.5 bg-blue-600 rounded-full mr-1.5 flex-shrink-0"></span>HTML</span>
+<span class="flex items-center text-sm font-medium text-gray-900 dark:text-white"><span class="flex w-2.5 h-2.5 bg-purple-500 rounded-full mr-1.5 flex-shrink-0"></span>CSS</span>
+<span class="flex items-center text-sm font-medium text-gray-900 dark:text-white"><span class="flex w-2.5 h-2.5 bg-indigo-500 rounded-full mr-1.5 flex-shrink-0"></span>JAVASCRIPT</span>
+<span class="flex items-center text-sm font-medium text-gray-900 dark:text-white"><span class="flex w-2.5 h-2.5 bg-teal-500 rounded-full mr-1.5 flex-shrink-0"></span>PHP</span>
+<span class="flex items-center text-sm font-medium text-gray-900 dark:text-white"><span class="flex w-2.5 h-2.5 bg-blue-600 rounded-full mr-1.5 flex-shrink-0"></span>EXPRES</span>
+<span class="flex items-center text-sm font-medium text-gray-900 dark:text-white"><span class="flex w-2.5 h-2.5 bg-purple-500 rounded-full mr-1.5 flex-shrink-0"></span>TAILWIND CSS</span>
+  </div>
+</div>
+
 
   </template>
